373d7872d52f5bff06385ae1df9b8c191bd25e71
Gabriel Biren <gbiren@ucla.edu>
2020-07-27 13:45:42 -0700

    Created test file, trouble with mount

--- pkg/localiso/localiso.go
+++ pkg/localiso/localiso.go
@@ -1,26 +1,35 @@
-package main
+package localiso
 
 import (
 	"context"
-	"log"
-	"os"
+	"fmt"
+	"io/ioutil"
 
+	"github.com/u-root/u-root/pkg/boot"
 	"github.com/u-root/u-root/pkg/boot/syslinux"
+	"github.com/u-root/u-root/pkg/mount"
+	"golang.org/x/sys/unix"
 )
 
-func main() {
-	args := os.Args[1:]
-	if len(args) != 1 {
-		log.Fatal("Expecting 1 argument.")
-		return
-	}
-	path := args[0]
-
-	iso, err := os.Open(path)
+// ParseConfigFromISO mounts an iso file to a
+// temp dir to get the config options
+func ParseConfigFromISO(isoPath string) ([]boot.OSImage, error) {
+	tmp, err := ioutil.TempDir("", "mnt")
 	if err != nil {
-		log.Fatal(err)
+		return nil, fmt.Errorf("Error creating mount dir: %v", err)
 	}
-	defer iso.Close()
 
-	syslinux.ParseLocalConfig(context.Background(), path)
+	if _, err := mount.Mount(isoPath, tmp, "iso9660", "--bind", unix.MS_RDONLY|unix.MS_NOATIME); err != nil {
+		return nil, fmt.Errorf("Error mounting ISO: %v", err)
+	}
+
+	configOpts, sysErr := syslinux.ParseLocalConfig(context.Background(), tmp)
+	umountErr := mount.Unmount(tmp, true, false) //force umount
+	if sysErr != nil {
+		return nil, fmt.Errorf("Error parsing config: %v", sysErr)
+	} else if umountErr != nil {
+		return nil, fmt.Errorf("Error unmounting ISO: %v", umountErr)
+	} else { // success
+		return configOpts, nil
+	}
 }
